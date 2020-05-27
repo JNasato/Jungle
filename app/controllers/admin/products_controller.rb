b7d694b97401bfe0e4db40a9aed324977a24b9d4
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-05-27 21:30:37 +0000

    push order details and ui changes to github

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with username: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def index
     @products = Product.order(id: :desc).all
