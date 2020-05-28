1c9b1faae017aa653e79f1f870b092410ce9a085
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-05-28 17:25:19 +0000

    add new route and create functionality for admin::categories page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,15 +2,22 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def index
+    @categories = Category.order(:id).all
+    @product_count = Product.group(:category_id).count
   end
 
   def new
+    @category = Category.new
   end
 
   def create
-  end
+    @category = Category.new(category_params)
 
-  def destroy
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 
   private
