531153e8ae4c01e8711c3e93c6cdeb0db372fd91
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-06-02 23:14:02 +0000

    write spec to test that the add-to-cart functionality works

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor can add product to cart", type: :feature, js: true do
+  
+  #SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.sentence(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see individual product details" do
+
+    # ACT
+    visit root_path
+
+    cart = within('nav') { find('i.fa-shopping-cart').find(:xpath, '..') }
+    expect(cart).to have_text 'My Cart (0)'
+    
+    within('article.product', match: :first) { click_button 'Add' }
+
+    #DEBUG
+    puts page.html
+    
+    #VERIFY
+    expect(cart).to have_text 'My Cart (1)'
+  end
+end
